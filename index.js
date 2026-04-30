ac4e86cc53642cc9e5054941ae71c164aef20422
Aileen Naa Ayele <your-email@example.com>
2026-04-29 23:55:16 -0400

    Make root entrypoint detectable by host

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import express from 'express'
 import app from './api/index.js'
 
 const PORT = process.env.PORT || 5050
+
+void express
 
 app.listen(PORT, () => {
   console.log(`API running on port ${PORT}`)
